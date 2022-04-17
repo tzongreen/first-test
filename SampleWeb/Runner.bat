c7a20f100bb987f33d292d44b2d2e9a7fdcf7faa
tzongreen <37132560+tzongreen@users.noreply.github.com>
2022-04-18 01:05:04 +0700

    Update Runner.bat

--- SampleWeb/Runner.bat
+++ SampleWeb/Runner.bat
@@ -1,2 +1,2 @@
-call mvn verify -Dcucumber.options="--tags @My_Telegram_Summary_Report"
+curl -s -X POST https://api.telegram.org/bot1835622636:AAFAql_uj9gVIkCNqnfnGsHMbqyNxwoQX7M/sendMessage -d chat_id=-784026545 -d text="Auto Test SERENITY.WEB >> FAILED"
 pause
